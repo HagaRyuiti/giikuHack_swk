759daf561c6123572c9f5c0b755d1bef0136a473
Ryuuichi <131429001+HagaRyuiti@users.noreply.github.com>
2025-01-28 21:18:11 +0900

    databasetest

---
+++ ryuuichi/create.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+print(roomcreate(test))
+
+def roomcreate(roomname, ):
+    #roomの作成、アクセス
+    room = (roomname + ".db")
+    conn = sqlite3.connect(room)
+    #sqliteを操作するカーソルオブジェクトの生成
+    cur = conn.cursor()
+
+    #roomテーブルの作成
+    cur.execute("CREATE TABLE room(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)")
+
+    #SQL文の実行
+    conn.commit()
+    #データベースにコミット
+    conn.close()
+
+    sql = "SELECT name FROM sqlite_master WHERE TYPE='table'"
+
+    for t in cursor.execute(sql):
+        print(t)
