887091de376ba72c44670e47401cb5c08cc1b0f3
Ryuuichi <131429001+HagaRyuiti@users.noreply.github.com>
2025-01-28 20:03:11 +0900

    試し

--- ryuuichi/app.py
+++ ryuuichi/app.py
@@ -2,12 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#ホーム画面
+#ホーム画面a
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
-#
+#アカウント画面
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+#検索画面
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+#作成画面
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
