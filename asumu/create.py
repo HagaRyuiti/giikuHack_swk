bead5b01d30505d4c497842a5bb5686074816e50
Ryuuichi <131429001+HagaRyuiti@users.noreply.github.com>
2025-01-31 20:17:22 +0900

    timeデータ追加

--- asumu/create.py
+++ asumu/create.py
@@ -13,8 +13,7 @@ def roomcreate(roomname, owner_id=1):
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT UNIQUE NOT NULL,
-                password TEXT NOT NULL,
-                email TEXT UNIQUE NOT NULL
+                time TIME UNIQUE NOT NULL
             );
         """)
 
